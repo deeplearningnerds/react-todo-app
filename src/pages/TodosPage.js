4805ef0a8da3c92b7a23c0ef559c6103b5130d51
Manuel Hanik <manuel.hanik@gmx.de>
2021-12-29 17:29:19 +0100

    Style Components

--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -7,7 +7,7 @@ const TodosPage = () => {
         <div className={"todos"}>
             <div className={"todos-header"}>
                 <h2 className={"todos-title"}>&#9782;Todos</h2>
-                <p className={"todos-count"}>{todos.length}</p>
+                <h2 className={"todos-title"}>{todos.length}</h2>
             </div>
             <div className={"todos-list"}>
                 {todos.map((todo, index) => (
