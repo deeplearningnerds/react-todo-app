6de29fb3928cc343cd30ed97b07fca198b7c8764
Manuel Hanik <manuel.hanik@gmx.de>
2021-12-31 15:12:12 +0100

    Added db.json

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import todos from "../assets/data";
+import React, {useState, useEffect} from 'react';
+//import todos from "../assets/data";
 import {ReactComponent as ArrowLeft} from "../assets/arrow-left.svg";
 import {useParams, Link} from "react-router-dom";
 
 const TodoPage = () => {
     let params = useParams();
     let todoId = Number(params.id);
-    let todo = todos.find(todo => todo.id == todoId)
+
+    let [todo, setTodo] = useState(null)
+
+    useEffect(() => {
+        getTodo()
+    }, [todoId])
+
+    //let todo = todos.find(todo => todo.id == todoId)
+
+    let getTodo = async () => {
+        let resonse = await fetch(`http://localhost:5000/todos/${todoId}`)
+        let data = await resonse.json()
+        setTodo(data)
+    }
+
     return (
         <div className={"todo"}>
             <div className={"todo-header"}>
--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import todos from "../assets/data";
+import React, {useState, useEffect} from 'react';
+//import todos from "../assets/data";
 import ListItem from "../components/ListItem";
 
 const TodosPage = () => {
+
+    let [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        getTodos()
+    }, [])
+
+    let getTodos = async () => {
+        let response = await fetch('http://localhost:5000/todos')
+        let data = await response.json()
+
+        setTodos(data)
+    }
+
     return (
         <div className={"todos"}>
             <div className={"todos-header"}>
